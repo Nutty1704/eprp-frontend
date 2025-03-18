46cff0513f6ac9978a782cc2e450b65df094da94
Abhijit Upadhyay <abhijitupadhyay03@gmail.com>
2025-03-18 23:40:14 +1100

    Add static assets for form. Add toaster component to App. Initialize router. Import and add classes for Inter and Rubik fonts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 function App() {
   return (
     <>
+      <Toaster
+        expand
+        position='bottom-right'
+        richColors
+      />
+      <Routes>
+
+      </Routes>
     </>
   )
 }
