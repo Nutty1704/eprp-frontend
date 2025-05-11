b9a28ac85cc6dd731b395d76e7ee5e5e339e2873
Goh Kai Wen <gohkw82@gmail.com>
2025-05-12 00:53:53 +1000

    Resized Logo and header links

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -12,21 +12,21 @@ const Header = ({ isOwner = false }) => {
   return (
     <header className="bg-white shadow-sm sticky top-0 z-50">
       <div className=" mx-auto px-4 sm:px-6 ">
-        <div className="flex justify-between h-16 items-center">
+        <div className="flex justify-between h-16 xl:h-24 items-center">
           <div>
             <Link to="/" className="flex items-center">
-              <Logo className="h-16 w-auto" theme='light' />
+              <Logo className="h-16 xl:h-24 w-auto" theme='light' />
             </Link>
           </div>
           
           <div className="flex items-center space-x-4">
-            <Link to="/" className="text-gray-900 hover:text-red-600 px-3 py-2 font-medium">
+            <Link to="/" className="text-gray-900 hover:text-red-600 px-3 py-2 font-medium text-base xl:text-xl">
               Home
             </Link>
             
 
             {isAuthenticated ? (
-              <Link to="/profile" className="text-gray-900 hover:text-red-600 px-3 py-2 font-medium">
+              <Link to="/profile" className="text-gray-900 hover:text-red-600 px-3 py-2 font-medium text-base xl:text-xl">
                 Profile
               </Link>
             ) : (
@@ -35,13 +35,13 @@ const Header = ({ isOwner = false }) => {
             
             {isAuthenticated ? (
               <Logout redirect={isOwner && '/' }>
-                <Button className="bg-primary hover:brightness-90 text-primary-foreground px-4 py-2 rounded">
+                <Button className="bg-primary hover:brightness-90 text-primary-foreground px-4 py-2 xl:px-5 xl:py-6 rounded text-base xl:text-xl">
                   Logout
                 </Button>
               </Logout>
             ) : (
               <AuthDialog isOwner={isOwner}>
-                <Button className="bg-primary hover:brightness-90 text-primary-foreground px-4 py-2 rounded">
+                <Button className="bg-primary hover:brightness-90 text-primary-foreground px-4 py-2 xl:px-5 xl:py-6 rounded text-base xl:text-xl">
                   Sign In
                 </Button>
               </AuthDialog>
