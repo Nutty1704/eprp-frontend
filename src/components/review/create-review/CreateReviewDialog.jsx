89426ba569d5fe4c3d292df22ac4e43eb88c52c5
Abhijit Upadhyay <abhijitupadhyay03@gmail.com>
2025-06-06 01:15:49 +1000

    Make dialog responsive

--- src/components/review/create-review/CreateReviewDialog.jsx
+++ src/components/review/create-review/CreateReviewDialog.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Dialog, DialogContent, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import ReviewForm from './ReviewForm';
 import ReviewSuccess from "./ReviewSuccess";
 import { createSuccessMessages as successMessages } from '@/src/config/Review';
+import useReviewStore from '@/src/stores/review-store';
 
 const MESSAGE_DURATION = 5500; // 5.5 seconds
 
@@ -15,6 +16,7 @@ const CreateReviewDialog = ({ children, business }) => {
     const [open, setOpen] = useState(false);
     const [success, setSuccess] = useState(false);
     const [reviewData, setReviewData] = useState(null);
+    const { setResetDeck } = useReviewStore();
 
     const handleSuccess = (formData) => {
         setReviewData(formData);
@@ -25,11 +27,24 @@ const CreateReviewDialog = ({ children, business }) => {
         }, MESSAGE_DURATION + 1500);
     };
 
+    const handleClose = () => {
+        if (success) {
+            setSuccess(false);
+            setResetDeck(true);
+        }
+    }
+
+    useEffect(() => {
+        if (!open) {
+            handleClose();
+        }
+    }, [open]);
+
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>{children}</DialogTrigger>
             <DialogContent
-                className={`min-w-[30vw] min-h-[75vh] 2xl:min-h-[50vh] ${success && 'p-0'}`}
+                className={`min-w-[70vw] 2xl:min-w-[25vw] min-h-[75vh] md:min-h-[50vh] ${success && 'p-0'}`}
                 closeIconClass={success && 'hidden'}
             >
                 <DialogTitle className="sr-only">Write a Review</DialogTitle>
